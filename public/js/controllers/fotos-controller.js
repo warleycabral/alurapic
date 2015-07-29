716dfdd01c9ed6be92f0602915da4489f7547533
warleycabral <warley.cabral@gmail.com>
2015-07-28 23:30:56 -0300

    Commit inicial

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+
+    $scope.foto = {
+        url: 'http://www.fundosanimais.com/Minis/leoes.jpg',
+        titulo: 'Leãozinho'
+    };
+});
