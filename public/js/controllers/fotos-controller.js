201821a2f5e10fd5d149928bb3e6e68c3fe7f452
warleycabral <warley.cabral@gmail.com>
2015-08-11 00:57:42 -0300

    Removendo comentários

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -11,13 +11,4 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         console.log(erro);
     })
     
-    /*var promise = $http.get('v1/fotos');
-    
-    promise
-    .then(function(retorno){
-        $scope.fotos = retorno.data;
-    })
-    .catch(function(error){
-        console.log(error);
-    });*/
 });
